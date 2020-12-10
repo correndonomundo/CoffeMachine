9be1e625818c2e0bd01c29070c4a6e9b8562a6ca
correndonomundo <andra.stancu@gmail.com>
2020-12-10 23:55:17 +0200

    c
    c

---
+++ Problems/The secret enumeration/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        //checkSecret();
+    }
+
+
+/*   static int counter;
+
+
+    public static void checkSecret(){
+        for (Secret value: Secret.values()) {
+            if (value.toString().substring(0,4).equals("STAR")) {
+                counter++;
+            }
+
+        }
+       System.out.println(counter);
+       //return counter;
+    }*/
+}
